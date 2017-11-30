a47a5ba324f85bcf0334e59105d2e638cb89dd66
Reasercher <vidarmarel@hotmail.com>
2017-11-30 10:57:21 +0000

    eyda index

--- Lukaverkufni/app.py
+++ Lukaverkufni/app.py
@@ -2,6 +2,37 @@ import bottle
 from beaker.middleware import SessionMiddleware
 
 session_opts = {
+    'session.type': 'memory',
+    'session.cookie_expires': 300,
+    'session.auto': True
+}
+app = SessionMiddleware(bottle.app(), session_opts)
+@bottle.route("/")
+def index():
+    return bottle.template("index.tpl")
+def session_test():
+    s = bottle.request.environ.get('beaker.session')
+    s['text'] ='vara'
+    s.save()
+    bottle.redirect('/output')
+
+
+@bottle.route('/output')
+def session_output():
+    s = bottle.request.environ.get('beaker.session')
+    return s['text']
+
+@bottle.route("/add/<item>")
+def addtocart(item):
+    print(item)
+    return bottle.redirect("/cart")
+@bottle.route("/cart")
+def cart():
+    return
+
+bottle.run(app=app, host='localhost', port=5000, debug=True, reloader=True)
+
+session_opts = {
     'session.type': 'file',
     'session.cookie_expires': 300,
     'session.data_dir': './data',
